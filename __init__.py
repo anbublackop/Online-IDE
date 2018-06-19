1b1f69cbb83058a2cad112e72f62570c96545300
Siddharth Yadav <siddharth49.yadav@gmail.com>
2018-06-20 04:21:48 +0530

    getting source for compilation

--- __init__.py
+++ __init__.py
@@ -8,24 +8,26 @@ app = Flask (__name__)
 def home():
 	return render_template ('main.html')
 
-@app.route('/homepage')
+@app.route('/homepage/')
 def Homepage():
 	return "This is Homepage"
 
-@app.route('/Login')
+@app.route('/Login/')
 def login():
 	return render_template('Login.html')
 
-@app.route('/compile_and_run', methods = ['POST'])
+@app.route('/compile_and_run/', methods = ['POST'])
 def compile_and_run():
 
 	endpoint_run = "http://api.hackerearth.com/code/run/"
 	client_secret_key = "71f2e0924665da501e290e3a2d831581a9f3a451"
 
+	source = request.form['source']
+
 	data = {
     'client_secret': client_secret_key,
     'async': 0,
-    'source': "",
+    'source': source,
     'lang': "PYTHON",
     'time_limit': 5,
     'memory_limit': 262144,
@@ -33,7 +35,7 @@ def compile_and_run():
 
 	response = requests.post(endpoint_run, data = data)
 
-	return render_template('main.html', Result = response.json())
+	return render_template('main.html', Result = response.text)
 
 if __name__== '__main__':
 	app.run(debug = True)
