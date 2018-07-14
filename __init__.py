b06d57a84b3ba8f1d679d7a38efdcd1356eb9788
Siddharth Yadav <siddharth49.yadav@gmail.com>
2018-07-15 01:28:37 +0530

    v1.0 is ready to roll

--- __init__.py
+++ __init__.py
@@ -22,20 +22,26 @@ def home():
 
 @app.route('/checkLogin/', methods = ['POST'])
 def checkLogin():
+	
 	c, conn = connection()
 	username = request.form['username']
 	password = request.form['password']
 	x = c.execute("select username from users where username = (%s)",(username,))
+	
 	if (c.fetchone() is not None):
 		c.execute("select * from users where username = (%s) and password = (%s)",(username, password))
+		
 		if (c.fetchone() is not None):
 			session["logged_in"] = True
 			session["username"] = username
 			return redirect(url_for('home'))
+		
 		else:
 			flash("Password is incorrect!")
 			return render_template('Login.html')
+	
 	flash("Please create an account first to login!")		
+	
 	return render_template('Login.html')			
 
 @app.route('/login/')
@@ -44,9 +50,12 @@ def login():
 
 @app.route('/logout/')
 def logout():
+	
 	if session['logged_in'] and session['username']:
+		
 		session.pop('logged_in')
 		session.pop('username')
+	
 	return redirect(url_for('home'))
 
 @app.route('/compile_and_run/', methods = ['POST'])
@@ -57,7 +66,7 @@ def compile_and_run():
 	client_secret_key = "4a941cc902adaca23c1e67330856b697726c68f84c5a88ccd1bf5c4cb7568ea3"
 
 	source = request.form["source"]
-	input_received = request.form["input"]
+	input_received = request.form.get("input")
 
 	data = {
     'clientSecret': client_secret_key,
@@ -75,17 +84,20 @@ def compile_and_run():
 		
 		c.execute("select uid from users where username = (%s)", (session['username'],))
 		userid = c.fetchone()[0]
+				
 		if not request.form.get('uniqueid'):
 			uniqueid = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 			c.execute("insert into mycode (userid, createdon, data, uniqueid) values (%s,%s,%s,%s)", (userid, thwart(str(datetime.now())), thwart(str(source)), uniqueid))
+		
 		else:
 			c.execute("update mycode set data = (%s) where uniqueid = (%s)", (thwart(str(source)), str(request.form['uniqueid'])[:-1]))
-			print (request.form['uniqueid'],str(source))
+		
 		conn.commit()
-		print (json.loads(response.text))
+
 	return jsonify(json.loads(response.text))
 
 class RegistrationForm(Form):
+	
 	username = TextField('Username', [validators.Length(min = 4, max = 20)])
 	email = TextField('Email', [validators.Length(min = 4, max = 50)])
 	password = PasswordField('Password', [validators.Required(), validators.EqualTo('confirm', message = "Passwords don't match!")])
@@ -94,7 +106,9 @@ class RegistrationForm(Form):
 
 @app.route('/register/', methods = ['GET', 'POST'])
 def register_page():
+	
 	try:
+		
 		form = RegistrationForm(request.form)
 
 		if request.method == "POST" and form.validate():
@@ -135,6 +149,7 @@ def previousCodes():
 	c, conn = connection()
 
 	if request.method == "GET":
+		
 		uniqueid = request.args.get('hash')
 		c.execute("delete from mycode where uniqueid = (%s)",(uniqueid,))
 		conn.commit()
@@ -143,6 +158,7 @@ def previousCodes():
 	userid = c.fetchone()[0]
 	c.execute("select * from mycode where userid = (%s)",(userid,))
 	previous_codes_list = c.fetchall()
+	
 	return render_template('previouscodes.html', list = previous_codes_list)
 
 
