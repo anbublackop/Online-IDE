69f111ec251e64ac064b151de7a7e4af927f283b
Siddharth Yadav <siddharth49.yadav@gmail.com>
2018-07-17 15:23:47 +0530

    updated database details as per cleardb credentials

--- dbconnect.py
+++ dbconnect.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
 def connection():
-	conn = MySQLdb.connect(host='localhost', user = 'root', passwd = 'hashirama', db = 'Online_IDE')
+	conn = MySQLdb.connect(host='us-cdbr-iron-east-04.cleardb.net', user = 'b50f908f898101', passwd = '42c7163a', db = 'heroku_fa7e2ebc29221a0')
 	c = conn.cursor()
 	return c, conn
